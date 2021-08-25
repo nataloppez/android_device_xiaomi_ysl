52073256f49d1255c7daa8d431502f7149e189dd
Natã Lopes <natalopes_carvalho@hotmail.com>
2021-08-25 20:38:14 -0300

    ysl: Add new extra surfaceflinger tweaks

--- prop.mk
+++ prop.mk
@@ -296,6 +296,9 @@ vendor.qc2.use.bqpool=1
 
 # SurfaceFlinger
 PRODUCT_PROPERTY_OVERRIDES += \
+ro.surface_flinger.protected_contents=true \
+ro.surface_flinger.set_idle_timer_ms=9000 \
+ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
 debug.sf.early_phase_offset_ns=1500000 \
 debug.sf.early_app_phase_offset_ns=1500000 \
 debug.sf.early_gl_phase_offset_ns=3000000 \
